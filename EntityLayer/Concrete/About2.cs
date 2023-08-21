00879fd36a9d73fb84aead44e9ce22f16c6a3ea8
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-08-21 17:48:58 +0300

    Add project files.

---
+++ EntityLayer/Concrete/About2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class About2
+	{
+        public int About2ID { get; set; }
+        public string Titile1 { get; set; }
+        public string Titile2 { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
